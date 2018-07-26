706e2ca5230595928799cf8bac04ac975e1f280c
Konrad <konrad.szelepusta@gmail.com>
2018-07-26 20:01:30 +0200

    softdeletable

--- src/Contest/ContestBundle/Entity/Traits/SoftdeletableTrait.php
+++ src/Contest/ContestBundle/Entity/Traits/SoftdeletableTrait.php
@@ -7,15 +7,14 @@ use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity
- * @Gedmo\SoftDeleteable(fieldName="deletedAt", timeAware=false, hardDelete=true)
+ * @Gedmo\SoftDeleteable(fieldName="deletedAt", timeAware=false, hardDelete=false)
  */
 trait SoftdeletableTrait
 {
     /**
      * @var \DateTime
      *
-     * @Gedmo\Timestampable(on="create")
-     * @ORM\Column(name="deleted_at", type="datetime")
+     * @ORM\Column(name="deleted_at", type="datetime", nullable=true)
      */
     private $deletedAt;
 }
